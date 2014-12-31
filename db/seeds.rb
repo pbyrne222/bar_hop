c5b2de0034e81b8fc91db86d6bc69fd9afd93493
Pete <petermason.byrne@gmail.com>
2014-12-31 15:16:14 -0700

    added seed data and script files

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Group.create([
+  { name: 'Percy Miracles'},
+  { name: 'Blackhawk Bayou'},
+  { name: 'Budweisder Bingos'},
+  { name: 'IPA Limas'},
+  ])
